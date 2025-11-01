20b60f77f9e1b128b64b56a0e0fb40fe47dd3ea2
ibrahimyagar <yafestahl@gmail.com>
2025-11-01 17:08:31 +0300

    SignalR servisleri Program.cs'e eklendi
    - builder.Services.AddSignalR() eklendi
    - WebRtcHub iÃ§in gerekli servisler artÄ±k kayÄ±tlÄ±

--- Program.cs
+++ Program.cs
@@ -18,6 +18,7 @@ namespace EduHub
             builder.Services.AddDefaultIdentity<IdentityUser>(options => options.SignIn.RequireConfirmedAccount = true)
                 .AddEntityFrameworkStores<ApplicationDbContext>();
             builder.Services.AddControllersWithViews();
+            builder.Services.AddSignalR();
            
 
             var app = builder.Build();
